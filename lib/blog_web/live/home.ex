a0bc9ada8dedccd67bf4880052b76bffdd75f5b9
Hrishikesh Choudhari <me@rishi.xyz>
2024-04-24 04:17:51 +0100

    Margin bottom for paragraphs in the post body

--- lib/blog_web/live/home.ex
+++ lib/blog_web/live/home.ex
@@ -16,8 +16,8 @@ defmodule BlogWeb.Home do
         <ul>
         <%= for post <- @posts do %>
           <li class="mt-14 ">
-            <h3 class="text-neutral-850 text-4xl font-snpro mb-3 tracking-tighter"><%= post.title %></h3>
-            <div class="text-neutral-950 tracking-tight text-xl font-snpro font-medium "><%= raw post.body %></div>
+            <h3 class="text-neutral-850 text-4xl font-snpro mb-10 tracking-tighter"><%= post.title %></h3>
+            <div class="text-neutral-950 tracking-tight text-xl font-snpro font-medium post-body"><%= raw post.body %></div>
           </li>
         <% end %>
         </ul>
