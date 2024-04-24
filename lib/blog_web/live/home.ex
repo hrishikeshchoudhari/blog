0ff4390bb2d25a4cf2ec82cfbde6f9cc4d1f2501
Hrishikesh Choudhari <me@rishi.xyz>
2024-04-24 04:04:05 +0100

    Fixed menubar judder & moved header graphic

--- lib/blog_web/live/home.ex
+++ lib/blog_web/live/home.ex
@@ -10,62 +10,7 @@ defmodule BlogWeb.Home do
 
     def render(assigns) do
       ~H"""
-      <div id="header-section">
-        <h1 class="mt-10 text-black text-4xl font-snpro mb-10 text-center font-medium">
-        Professionally experienced in Product Management and Cloud Systems Architecture. Managed teams of 25+ members distributed globally.
-        </h1>
-        <ul>
-        <li class="mb-1">
-          <div class="flex flex-row">
-              <div class="text-3xl w-36 text-teal-500 font-snpro font-medium tracking-tight">Code</div>
-              <div class="flex-col h-5 w-full pl-1">
-                <div class="w-full h-1 bg-gradient-to-r from-teal-500 to-teal-950 mb-1 rounded-l-lg"></div>
-                <div class="w-full h-1 bg-gradient-to-r from-teal-500 to-teal-950 mb-1 rounded-l-lg"></div>
-                <div class="w-full h-1 bg-gradient-to-r from-teal-500 to-teal-950 mb-1 rounded-l-lg"></div>
-                <div class="w-full h-1 bg-gradient-to-r from-teal-500 to-teal-950 mb-1 rounded-l-lg"></div>
-                <div class="w-full h-1 bg-gradient-to-r from-teal-500 to-teal-950 mb-1 rounded-l-lg"></div>
-              </div>
-            </div>
-          </li>
-          <li class="mb-1">
-            <div class="flex flex-row">
-              <div class="text-3xl w-36 text-amber-600 font-snpro font-medium tracking-tight">People</div>
-              <div class="flex-col h-5 w-full pl-1">
-                <div class="w-full h-1 bg-gradient-to-r from-amber-600 to-amber-950 mb-1 rounded-l-lg"></div>
-                <div class="w-full h-1 bg-gradient-to-r from-amber-600 to-amber-950 mb-1 rounded-l-lg"></div>
-                <div class="w-full h-1 bg-gradient-to-r from-amber-600 to-amber-950 mb-1 rounded-l-lg"></div>
-                <div class="w-full h-1 bg-gradient-to-r from-amber-600 to-amber-950 mb-1 rounded-l-lg"></div>
-                <div class="w-full h-1 bg-gradient-to-r from-amber-600 to-amber-950 mb-1 rounded-l-lg"></div>
-              </div>
-            </div>
-          </li>
 
-          <li class="mb-1">
-          <div class="flex flex-row">
-              <div class="text-3xl w-36 text-purple-600 font-snpro font-medium tracking-tight">Design</div>
-              <div class="flex-col h-5 w-full pl-1">
-                <div class="w-full h-1 bg-gradient-to-r from-purple-600 to-purple-950 mb-1 rounded-l-lg"></div>
-                <div class="w-full h-1 bg-gradient-to-r from-purple-600 to-purple-950 mb-1 rounded-l-lg"></div>
-                <div class="w-full h-1 bg-gradient-to-r from-purple-600 to-purple-950 mb-1 rounded-l-lg"></div>
-                <div class="w-full h-1 bg-gradient-to-r from-purple-600 to-purple-950 mb-1 rounded-l-lg"></div>
-                <div class="w-full h-1 bg-gradient-to-r from-purple-600 to-purple-950 mb-1 rounded-l-lg"></div>
-              </div>
-            </div>
-          </li>
-          <li class="mb-16">
-          <div class="flex flex-row">
-              <div class="text-3xl w-36 text-sky-600 font-snpro font-medium tracking-tight">Process</div>
-              <div class="flex-col h-5 w-full pl-1">
-                <div class="w-full h-1 bg-gradient-to-r from-sky-600 to-sky-950 mb-1 rounded-l-lg"></div>
-                <div class="w-full h-1 bg-gradient-to-r from-sky-600 to-sky-950 mb-1 rounded-l-lg"></div>
-                <div class="w-full h-1 bg-gradient-to-r from-sky-600 to-sky-950 mb-1 rounded-l-lg"></div>
-                <div class="w-full h-1 bg-gradient-to-r from-sky-600 to-sky-950 mb-1 rounded-l-lg"></div>
-                <div class="w-full h-1 bg-gradient-to-r from-sky-600 to-sky-950 mb-1 rounded-l-lg"></div>
-              </div>
-            </div>
-          </li>
-        </ul>
-      </div>
 
       <div class="posts">
         <ul>
